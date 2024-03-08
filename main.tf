4cd36f47b087710fd843709d8e52385aa2f5e2fa
Sachin Kumar <Sachin.Kumar@cantire.com>
2024-03-08 22:44:33 +0530

    Add files

---
+++ main.tf
@@ -0,0 +1,37 @@
+# configure the azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+  cloud {
+    organization = "kumarsachin437"
+
+    workspaces {
+      name = "my-TF-workspace"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = "westus2"
+}
+
+variable "rg_name" {
+  description = "Name of resource group"
+  default     = "DefaultRGName"
+
+}
+output "resource_group_name" {
+  description = "Name of Resource group"
+  value       = azurerm_resource_group.rg.name
+
+}
